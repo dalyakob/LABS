62072f24204a4538c758280d6b68224912969042
dalyakob <dalyakob@umich.edu>
2020-04-16 21:45:23 -0400

    Completed Lab 2git status

---
+++ Lab_2/Lab_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string condition;
+
+            Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!\n");
+
+            do
+            {
+                int length, width;
+                while (true) {
+                    try
+                    {
+                        Console.Write("Enter Length: ");
+                        length = int.Parse(Console.ReadLine());
+
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid Length, Please Try Again!");
+                    }
+                }
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Enter width: ");
+                        width = int.Parse(Console.ReadLine());
+
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid Width, Please Try Again!");
+                    }
+                }
+
+                Console.WriteLine("\nPerimeter: " + (width + length));
+
+                Console.WriteLine("Area: " + (width * length));
+
+                Console.WriteLine();
+
+                Console.WriteLine("Would you like to measure another room? (y/n)");
+                condition = Console.ReadLine().ToLower();
+
+                } while (condition == "y");
+
+            Console.WriteLine("\nThanks for using this our Room Detail Generator, goodbye!!! :)");
+        }
+    }
+}
