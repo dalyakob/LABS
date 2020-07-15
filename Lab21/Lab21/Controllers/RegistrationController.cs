54bf2a947f640da81a867540a714154a758d36aa
dalyakob <dalyakob@umich.edu>
2020-07-15 18:54:17 -0400

    Lab 21 Complete

---
+++ Lab21/Lab21/Controllers/RegistrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab20.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab20.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Summary(Registration registration)
+        {
+            return View(registration);
+        }
+    }
+}
---
+++ Lab21/Lab21/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab20.Models
+{
+    public class Registration
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
